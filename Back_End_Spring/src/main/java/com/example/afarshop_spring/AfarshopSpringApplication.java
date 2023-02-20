cbe8c90422dc90dcf3845369364be4896c55d215
Ismail-khlif <93547849+Ismail-khlif@users.noreply.github.com>
2023-02-20 13:20:11 +0100

    Environement Back-end Spring Setup

---
+++ Back_End_Spring/src/main/java/com/example/afarshop_spring/AfarshopSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.afarshop_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfarshopSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfarshopSpringApplication.class, args);
+    }
+
+}
